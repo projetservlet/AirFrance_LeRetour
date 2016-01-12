18ffd44a73a9e0f1b2489ca020a23e42cda99819
j4g0n <simon.andreux@gmail.com>
2016-01-12 16:17:43 +0100

    add bullshit servlet

---
+++ src/controllers/SrvBidon.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import java.io.IOException;
+
+/**
+ * Created by ipi on 12/01/2016.
+ */
+@javax.servlet.annotation.WebServlet("/SrvBidon")
+public class SrvBidon extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
