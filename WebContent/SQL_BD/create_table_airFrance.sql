d2bb6668058d13c847de56c5aa63fae1e6590a3f
quentinkieffer <quentin.kieffer@yahoo.fr>
2016-01-13 14:17:13 +0100

    creation des table dans l'ordre

--- WebContent/SQL_BD/create_table_airFrance.sql
+++ WebContent/SQL_BD/create_table_airFrance.sql
@@ -1,3 +1,20 @@
+CREATE TABLE `pays` (
+  `idPays` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(3) DEFAULT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`idPays`),
+  UNIQUE KEY `code_UNIQUE` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ville` (
+  `idVille` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(60) CHARACTER SET utf8 DEFAULT NULL,
+  `pays_idPays` int(11) NOT NULL,
+  PRIMARY KEY (`idVille`),
+  KEY `fk_ville_pays1_idx` (`pays_idPays`),
+  CONSTRAINT `fk_ville_pays1` FOREIGN KEY (`pays_idPays`) REFERENCES `pays` (`idPays`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE `aeroport` (
   `idAeroport` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -26,16 +43,6 @@ CREATE TABLE `client` (
   UNIQUE KEY `mail_UNIQUE` (`mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `etape` (
-  `idReservation` int(11) NOT NULL,
-  `idVol` int(11) NOT NULL,
-  `numero` int(11) DEFAULT NULL,
-  `type` enum('aller','retour') COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`idReservation`,`idVol`),
-  KEY `fk_etape_vol1_idx` (`idVol`),
-  CONSTRAINT `fk_etape_reservation1` FOREIGN KEY (`idReservation`) REFERENCES `reservation` (`idReservation`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_etape_vol1` FOREIGN KEY (`idVol`) REFERENCES `vol` (`idVol`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `passager` (
   `idPassager` int(11) NOT NULL AUTO_INCREMENT,
@@ -47,13 +54,6 @@ CREATE TABLE `passager` (
   CONSTRAINT `fk_passager_reservation1` FOREIGN KEY (`reservation_idReservation`) REFERENCES `reservation` (`idReservation`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `pays` (
-  `idPays` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(3) DEFAULT NULL,
-  `nom` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idPays`),
-  UNIQUE KEY `code_UNIQUE` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `reservation` (
   `idReservation` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,15 +64,6 @@ CREATE TABLE `reservation` (
   PRIMARY KEY (`idReservation`),
   KEY `fk_reservation_client1_idx` (`client_idClient`),
   CONSTRAINT `fk_reservation_client1` FOREIGN KEY (`client_idClient`) REFERENCES `client` (`idClient`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `ville` (
-  `idVille` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(60) CHARACTER SET utf8 DEFAULT NULL,
-  `pays_idPays` int(11) NOT NULL,
-  PRIMARY KEY (`idVille`),
-  KEY `fk_ville_pays1_idx` (`pays_idPays`),
-  CONSTRAINT `fk_ville_pays1` FOREIGN KEY (`pays_idPays`) REFERENCES `pays` (`idPays`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `vol` (
@@ -92,4 +83,15 @@ CREATE TABLE `vol` (
   CONSTRAINT `fk_vol_aeroport` FOREIGN KEY (`id_aeroport_depart`) REFERENCES `aeroport` (`idAeroport`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_vol_aeroport1` FOREIGN KEY (`id_aeroport_arrivee`) REFERENCES `aeroport` (`idAeroport`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_vol_avion1` FOREIGN KEY (`avion_idAvion`) REFERENCES `avion` (`idAvion`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `etape` (
+  `idReservation` int(11) NOT NULL,
+  `idVol` int(11) NOT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `type` enum('aller','retour') COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`idReservation`,`idVol`),
+  KEY `fk_etape_vol1_idx` (`idVol`),
+  CONSTRAINT `fk_etape_reservation1` FOREIGN KEY (`idReservation`) REFERENCES `reservation` (`idReservation`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_etape_vol1` FOREIGN KEY (`idVol`) REFERENCES `vol` (`idVol`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
