ba6c9281ffcdba391df5f996acc526084509beda
Baune Francois <francois.baune@gmail.com>
2016-01-14 14:31:57 +0100

    Update SingupServlet (check email in db)

--- src/controllers/SignupServlet.java
+++ src/controllers/SignupServlet.java
@@ -10,6 +10,7 @@ import models.Client;
 import models.HibernateHelper;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,55 +19,64 @@ import java.util.regex.Pattern;
  */
 @WebServlet("/SignupServlet")
 public class SignupServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String firstname          = request.getParameter("firstname");
-        String lastname           = request.getParameter("lastname");
-        String email              = request.getParameter("email");
-        String confirmEmail       = request.getParameter("confirmEmail");
-        String password           = request.getParameter("password");
-        String confirmPassword    = request.getParameter("confirmPassword");
-           
-        boolean ClientCanBeCreated = true;
-        
-        //check password is correct
-        Pattern p = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$");
-        Matcher m = p.matcher(password);
-        if (!m.matches() ) {
-        	ClientCanBeCreated = false;
-        	request.setAttribute("errorMessagepassword","Mot de passe incorrect : doit contenir au moin une majuscule, une miniscule et un chiffre");
-        }
-        if (!password.equals(confirmPassword)) {
-        	ClientCanBeCreated = false;
-        	request.setAttribute("errorMessageconfirmPassword","vous avez rentré deux mots de passe différents");
-        }
-        
-        if (!email.equals(confirmEmail)) {
-        	ClientCanBeCreated = false;
-        	request.setAttribute("confirmEmail","vous avez rentré deux emails différents");
-        }
-      
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		String confirmEmail = request.getParameter("confirmEmail");
+		String phone = request.getParameter("phone");
+		String address = request.getParameter("address");
+		String password = request.getParameter("password");
+		String confirmPassword = request.getParameter("confirmPassword");
 
-        if (ClientCanBeCreated) {
-        	
-        	Client newClient = new Client(email);
-        	newClient.setNom(lastname);
-        	newClient.setPrenom(firstname);
-        	//newClient.setNom(password);
-        	HibernateHelper.AddObjectInDB(newClient);
+		boolean ClientCanBeCreated = true;
 
-        	
-        	
-        getServletContext().getRequestDispatcher("/RedirectHomepageServlet").forward(request, response);
-        }
-        else {
-         getServletContext().getRequestDispatcher("/Signup.jsp").forward(request, response);
-        }
+		// check password is correct
+		Pattern p = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$");
+		Matcher m = p.matcher(password);
+		if (!m.matches()) {
+			ClientCanBeCreated = false;
+			request.setAttribute("errorMessagepassword",
+					"Mot de passe incorrect : doit contenir au moin une majuscule, une miniscule et un chiffre");
+		}
+		if (!password.equals(confirmPassword)) {
+			ClientCanBeCreated = false;
+			request.setAttribute("errorMessageconfirmPassword", "vous avez rentré deux mots de passe différents");
+		}
 
-    }
+		if (!email.equals(confirmEmail)) {
+			ClientCanBeCreated = false;
+			request.setAttribute("errorMessageconfirmEmail", "vous avez rentré deux emails différents");
+		}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String url = "/Signup.jsp";
-//        response.sendRedirect(url);
+		ArrayList<Client> listeClient = HibernateHelper.Clients();
+		for (Client c : listeClient) {
+			if (c.getMail().equals(email)) {
+				ClientCanBeCreated = false;
+				request.setAttribute("errorMessageemail", "Un compte utilise déjà cet email !");
+			}
+		}
 
-    }
+		if (ClientCanBeCreated) {
+
+			Client newClient = new Client(email);
+			newClient.setNom(lastname);
+			newClient.setPrenom(firstname);
+			newClient.setTelephone(phone);
+			newClient.setAdresse(address);
+			HibernateHelper.AddObjectInDB(newClient);
+			getServletContext().getRequestDispatcher("/RedirectHomepageServlet").forward(request, response);
+		} else {
+			getServletContext().getRequestDispatcher("/Signup.jsp").forward(request, response);
+		}
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// String url = "/Signup.jsp";
+		// response.sendRedirect(url);
+
+	}
 }
